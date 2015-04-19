a1de4f952fd36a6ed3b16045eedc1fbdbb428880
Slava Shklyaev <shk.slava@gmail.com>
2015-04-19 14:48:18 +0300

    Remove parens

--- A.cpp
+++ A.cpp
@@ -164,6 +164,7 @@ string normalize_code(string code) {
     code = filter([](char c) { return !isspace(c); }, code);
     code = normalize_numbers(code);
     code = filter([](char c) { return c != ';'; }, code);
+    code = filter([](char c) { return c != '{' && c != '}' && c != '(' && c != ')'; }, code);
     return code;
 }
 
