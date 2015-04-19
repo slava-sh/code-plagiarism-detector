9274669d194f217ba9b4385444c79e5ef47fd044
Slava Shklyaev <shk.slava@gmail.com>
2015-04-19 23:35:38 +0300

    Tune

--- A.cpp
+++ A.cpp
@@ -11,8 +11,8 @@
 #include <algorithm>
 using namespace std;
 
-const int MAX_DIST = 400;
-const double MAX_DIST_RATIO = 0.2;
+const int MAX_DIST = 1000; // Doesn't matter
+const double MAX_DIST_RATIO = 0.25;
 
 string read_file(const char *filename) {
     ifstream in(filename, ios::binary);
